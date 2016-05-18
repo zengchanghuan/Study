ba3cc626c2e8597d1ce43e915c34e1bfe807d483
zengchanghuan <zengchanghuan@gmail.com>
2016-05-18 16:58:23 +0800

    add file

---
+++ CoreLocation/CoreLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocation
+//
+//  Created by ZengChanghuan on 16/5/18.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreLocation/CoreLocation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreLocation
+//
+//  Created by ZengChanghuan on 16/5/18.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
