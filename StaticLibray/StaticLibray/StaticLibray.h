a765aa1e4720da8cba4bf6027d75c7325c416d7f
zengchanghuan <zengchanghuan@gmail.com>
2016-05-18 17:51:00 +0800

    add file

---
+++ StaticLibray/StaticLibray/StaticLibray.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibray.h
+//  StaticLibray
+//
+//  Created by ZengChanghuan on 16/5/18.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibray : NSObject
+
+@end
---
+++ StaticLibray/StaticLibray/StaticLibray.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibray.m
+//  StaticLibray
+//
+//  Created by ZengChanghuan on 16/5/18.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import "StaticLibray.h"
+
+@implementation StaticLibray
+
+@end
