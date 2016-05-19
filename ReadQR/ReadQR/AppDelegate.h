25936584c4312ca5678abf36c1c6a553c7ec000c
zengchanghuan <zengchanghuan@gmail.com>
2016-05-19 11:38:00 +0800

    二维码读取

---
+++ ReadQR/ReadQR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReadQR
+//
+//  Created by ZengChanghuan on 16/5/19.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReadQR/ReadQR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReadQR
+//
+//  Created by ZengChanghuan on 16/5/19.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
