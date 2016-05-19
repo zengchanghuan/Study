75ca6e464f53913f652978deb89df618df449f53
zengchanghuan <zengchanghuan@gmail.com>
2016-05-19 12:10:50 +0800

    扫描优化

--- ReadQR/ReadQR/ViewController.m
+++ ReadQR/ReadQR/ViewController.m
@@ -18,36 +18,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //实例化拍摄设备
+    [self readQR];
+}
+/**
+ *  扫描二维码;输入：由会话将摄像头采集到的二维码图像转换成字符串数据,输出数据，由预览图层显示扫描场景
+ */
+- (void)readQR
+{
+    // 1. 实例化拍摄设备
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
-    //设备输入设备
+    // 2. 设置输入设备
     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
     
-    //实例化拍摄元数据输出
+    // 3. 设置元数据输出
+    // 3.1 实例化拍摄元数据输出
     AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
-    //数据代理
+    // 3.3 设置输出数据代理
     [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    //实例化拍摄会话
+    
+    // 4. 添加拍摄会话
+    // 4.1 实例化拍摄会话
     AVCaptureSession *session = [[AVCaptureSession alloc] init];
-    
-    //添加会话输入
+    // 4.2 添加会话输入
     [session addInput:input];
-    
-    //添加会话输出
+    // 4.3 添加会话输出
     [session addOutput:output];
-    
-    //设置输出数据类型，需要将元数据输出添加到会话后，才能指定元数据类型，否则会报错
+    // 4.3 设置输出数据类型，需要将元数据输出添加到会话后，才能指定元数据类型，否则会报错
     [output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
     
-    //实例化预览图层
-    AVCaptureVideoPreviewLayer *preview = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    self.session = session;
+    
+    // 5. 视频预览图层
+    // 5.1 实例化预览图层, 传递_session是为了告诉图层将来显示什么内容
+    AVCaptureVideoPreviewLayer *preview = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    
     preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
     preview.frame = self.view.bounds;
+    // 5.2 将图层插入当前视图
     [self.view.layer insertSublayer:preview atIndex:100];
-    self.previewLayer = preview;
-    [session startRunning];
     
+    self.previewLayer = preview;
+    
+    // 6. 启动会话
+    [_session startRunning];
+
+    
+
 }
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
 {
