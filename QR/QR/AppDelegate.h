e0c27f1072a2d0d5a842b262a43092a5c0276f3a
zengchanghuan <zengchanghuan@gmail.com>
2016-05-19 10:08:58 +0800

    二维码

---
+++ QR/QR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QR
+//
+//  Created by ZengChanghuan on 16/5/19.
+//  Copyright © 2016年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
