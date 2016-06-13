7041b12b1480b75e782e04cc653b8a53e409c42f
zengchanghuan <zengchanghuan@gmail.com>
2016-06-13 15:36:27 +0800

    tabBar弹出菜单自定义

--- SportClub/SportClub/Classes/Main/Controller/XZTabBarViewController.m
+++ SportClub/SportClub/Classes/Main/Controller/XZTabBarViewController.m
@@ -14,6 +14,14 @@
 #import "XZStoreViewController.h"
 #import "XZUITabBar.h"
 #import "XZNavigationController.h"
+#import "MenuLabel.h"
+#import "HyPopMenuView.h"
+#import <POP.h>
+
+#define Objs @[[MenuLabel CreatelabelIconName:@"tabbar_compose_idea" Title:@"文字"],[MenuLabel CreatelabelIconName:@"tabbar_compose_photo" Title:@"相册"],[MenuLabel CreatelabelIconName:@"tabbar_compose_camera" Title:@"拍摄"],[MenuLabel CreatelabelIconName:@"tabbar_compose_lbs" Title:@"签到"],[MenuLabel CreatelabelIconName:@"tabbar_compose_review" Title:@"点评"],[MenuLabel CreatelabelIconName:@"tabbar_compose_more" Title:@"更多"],]
+
+
+
 @interface XZTabBarViewController ()<XZTabBarDelegate>
 
 @end
@@ -56,8 +64,37 @@
 }
 -(void)tabBarDidClickPlusButton:(XZUITabBar *)tabBar
 {
-    XZComposeViewController *compose = [XZComposeViewController new];
-    XZNavigationController *nav = [[XZNavigationController alloc] initWithRootViewController:compose];
-    [self presentViewController:nav animated:YES completion:nil];
+    
+    CGFloat x,y,w,h;
+    x = CGRectGetWidth(self.view.bounds)/2 - 213/2;
+    y = CGRectGetHeight([UIScreen mainScreen].bounds)/2 * 0.3f;
+    w = 213;
+    h = 57;
+    //自定义的头部视图
+    UIImageView *topView = [[ImageView alloc] initWithFrame:CGRectMake(x, y, w, h)];
+    topView.image = [UIImage imageNamed:@"compose_slogan"];
+    topView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    NSMutableDictionary *AudioDictionary = [NSMutableDictionary dictionary];
+    
+    //添加弹出菜单音效
+    [AudioDictionary setObject:@"composer_open" forKey:kHyPopMenuViewOpenAudioNameKey];
+    [AudioDictionary setObject:@"wav" forKey:kHyPopMenuViewOpenAudioTypeKey];
+    //添加取消菜单音效
+    [AudioDictionary setObject:@"composer_close" forKey:kHyPopMenuViewCloseAudioNameKey];
+    [AudioDictionary setObject:@"wav" forKey:kHyPopMenuViewCloseAudioTypeKey];
+    //添加选中按钮音效
+    [AudioDictionary setObject:@"composer_select" forKey:kHyPopMenuViewSelectAudioNameKey];
+    [AudioDictionary setObject:@"wav" forKey:kHyPopMenuViewSelectAudioTypeKey];
+    
+    [HyPopMenuView CreatingPopMenuObjectItmes:Objs TopView:topView /*nil*/OpenOrCloseAudioDictionary:AudioDictionary /*nil*/ SelectdCompletionBlock:^(MenuLabel *menuLabel, NSInteger index) {
+        NSLog(@"index:%ld ItmeNmae:%@",index,menuLabel.title);
+    }];
+    
+    
+    
+//    XZComposeViewController *compose = [XZComposeViewController new];
+//    XZNavigationController *nav = [[XZNavigationController alloc] initWithRootViewController:compose];
+//    [self presentViewController:nav animated:YES completion:nil];
 }
 @end
