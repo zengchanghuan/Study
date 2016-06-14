2a9bed7826846a40def0eff8ab5e06ae312b97da
zengchanghuan <zengchanghuan@gmail.com>
2016-06-14 15:45:44 +0800

    数据操作

--- sqlite/sqlite/ViewController.m
+++ sqlite/sqlite/ViewController.m
@@ -8,9 +8,12 @@
 
 #import "ViewController.h"
 #import <sqlite3.h>
-@interface ViewController ()
+#import "XZShop.h"
+@interface ViewController ()<UITableViewDataSource>
 @property (strong, nonatomic) IBOutlet UITextField *nameField;
 @property (strong, nonatomic) IBOutlet UITextField *priceField;
+@property (strong, nonatomic) NSMutableArray *shopArray;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
  //数据库对象实例
 @property (assign, nonatomic) sqlite3 *db;
 - (IBAction)insert;
@@ -19,16 +22,30 @@
 
 @implementation ViewController
 
+- (NSMutableArray *)shops
+{
+    if (!_shopArray) {
+        self.shopArray = [[NSMutableArray alloc] init];
+        
+    }
+    return _shopArray;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self setupDB];
+    [self setupData];
+}
+
+// 初始化数据库
+- (void)setupDB
+{
     //打开数据库，连接数据库
     
     NSString *filename = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"shops.sqlite"];
-    
+    NSLog(@"filename = %@",filename);
     //如果数据库文件不存在，系统会自动创建文件初始化数据库
     
-   
+    
     int status = sqlite3_open(filename.UTF8String, &_db);
     
     if (status == SQLITE_OK) {
@@ -41,11 +58,11 @@
         if (errmsg) {
             NSLog(@"创表失败--%s",errmsg);
         }
-
+        
     } else {
         NSLog(@"打开数据库失败");
     }
-  
+    
     
     
     
@@ -55,9 +72,53 @@
     //sqlite3_close(<#sqlite3 *#>)
 }
 
+/**
+ *  查询数据
+ */
+- (void)setupData
+{
+    const char *sql = "select name,price from t_shop;";
+    //取出查询结果的
+    sqlite3_stmt *stmt = NULL;
+    int status = sqlite3_prepare_v2(self.db, sql, -1, &stmt, NULL);
+    while (status == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            const  char *name = (const char *)sqlite3_column_text(stmt, 0);
+            const  char *price = (const char *)sqlite3_column_text(stmt, 1);
+            XZShop *shop = [[XZShop alloc] init];
+            shop.name = [NSString stringWithUTF8String:name];
+            shop.price = [NSString stringWithUTF8String:price];
+            [_shopArray addObject:shop];
+            NSLog(@"%s %s",name,price);
+        }
+    }
+}
 - (IBAction)insert {
     
     NSString *sql = [NSString stringWithFormat:@"insert into t_shop (name,price) values ('%@',%f)",self.nameField.text,self.priceField.text.doubleValue];
     sqlite3_exec(self.db, sql.UTF8String, NULL, NULL, NULL);
+    XZShop *shop = [XZShop new];
+    shop.name = self.nameField.text;
+    shop.price = self.priceField.text;
+    [_shopArray addObject:shop];
+    [self.tableView reloadData];
+}
+
+#pragma mark UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _shopArray.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"shop";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    XZShop *shop = _shopArray[indexPath.row];
+    cell.textLabel.text = shop.name;
+    cell.detailTextLabel.text = shop.price;
+    return cell;
 }
 @end
