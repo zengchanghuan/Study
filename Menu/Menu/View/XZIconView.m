5dc8e08afa093c4a98ebd839997d6bb563ecc4e5
zengchanghuan <zengchanghuan@gmail.com>
2016-06-15 20:05:51 +0800

    控制器切换和自动布局

--- Menu/Menu/View/XZIconView.m
+++ Menu/Menu/View/XZIconView.m
@@ -10,6 +10,36 @@
 
 @implementation XZIconView
 
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self setImage:[UIImage imageNamed:@"lufy"] forState:UIControlStateNormal];
+        [self setTitle:@"哈哈" forState:UIControlStateNormal];
+        
+        self.imageView.layer.cornerRadius = 10;
+        self.titleLabel.textAlignment = NSTextAlignmentCenter;
 
+    }
+    return self;
+}
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    if (Lanscape) {
+        self.imageView.y = 0;
+        self.imageView.width = self.width;
+        self.imageView.height = self.width;
+        
+        self.titleLabel.hidden = NO;
+        self.titleLabel.x = 0;
+        self.titleLabel.y = self.imageView.height;
+        self.titleLabel.width = self.width;
+        self.titleLabel.height = self.height - self.imageView.height;
+    } else { // 竖屏
+        self.titleLabel.hidden = YES;
+        self.imageView.frame = self.bounds;
+    }
+}
 
 @end
--- Menu/Menu/XZHomeViewController.m
+++ Menu/Menu/XZHomeViewController.m
@@ -8,31 +8,85 @@
 
 #import "XZHomeViewController.h"
 #import "XZDock.h"
-
+#import "Masonry.h"
 
 @interface XZHomeViewController ()
 @property (weak, nonatomic) XZDock *dock;
+@property (weak, nonatomic) UIViewController *showingChildVC;
 @end
 
 @implementation XZHomeViewController
 
+- (XZDock *)dock
+{
+    if (!_dock) {
+        XZDock *dock = [XZDock new];
+        [self.view addSubview:dock];
+        self.dock = dock;
+        
+    }
+    return _dock;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    XZDock *dock = [XZDock new];
-    dock.width = 150;
-    dock.height = self.view.height;
-    _dock = dock;
-    [self.view addSubview:dock];
+    
+    // 设置背景色
+    self.view.backgroundColor = HMColor(55, 55, 55);
+    
+    // 初始化子控制器
+    [self setupChildVcs];
+    
+    // 监听通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tabBarDidSelect:) name:HMTabBarDidSelectNotification object:nil];
+    
     
     //根据屏幕方向设置dock属性
     [self willRotateToInterfaceOrientation:self.interfaceOrientation duration:0];
 }
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
-//-(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
-//{
-//    
-//}
+- (void)tabBarDidSelect:(NSNotification *)notification
+{
+    int index = [notification.userInfo[HMTabBarSelectIndex] intValue];
+    
+    [self switchChildVc:index];
+}
 
+- (void)setupChildVcs{
+    for (int i = 0; i < 6; i++) {
+        UIViewController *vc = [UIViewController new];
+        vc.view.backgroundColor = HMRandomColor;
+        [self addChildViewController:vc];
+    }
+    [self switchChildVc:0];
+}
+
+/**
+ *  切换子控制器
+ *
+ *  @param index 最新子控制器的索引
+ */
+
+- (void)switchChildVc:(int)index
+{
+    [self.showingChildVC.view removeFromSuperview];
+    
+    UIViewController *newChildVC = self.childViewControllers[index];
+    [self.view addSubview:newChildVC.view];
+    self.showingChildVC = newChildVC;
+    
+    [newChildVC.view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.view.mas_right);
+        make.top.equalTo(self.view.mas_top);
+        make.bottom.equalTo(self.view.mas_bottom);
+        make.left.equalTo(self.dock.mas_right);
+    }];
+    
+}
 
 -(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
 {
