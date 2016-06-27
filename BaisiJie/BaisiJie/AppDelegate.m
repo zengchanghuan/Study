cdff8534d0bd0d9fb6802bf73b64f0c57f414070
zengchanghuan <zengchanghuan@gmail.com>
2016-06-27 20:17:10 +0800

    base config

--- BaisiJie/BaisiJie/AppDelegate.m
+++ BaisiJie/BaisiJie/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    self.window.rootViewController = [ViewController new];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
