7fbd1f2d842e5649a51e412b081cd4a3467ef0fc
zengchanghuan <zengchanghuan@gmail.com>
2016-06-28 08:54:21 +0800

    customer NavBar

--- BaisiJie/BaisiJie/XZTabBarController.m
+++ BaisiJie/BaisiJie/XZTabBarController.m
@@ -55,8 +55,10 @@
     vc.tabBarItem.image = [UIImage imageNamed:image];
     vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
     vc.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(100)/100.0 green:arc4random_uniform(100)/100.0 blue:arc4random_uniform(100)/100.0 alpha:1.0];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+
     // 添加为子控制器
-    [self addChildViewController:vc];
+    [self addChildViewController:nav];
 }
 
 @end
