e5b50aaf926d429d3396a901db1424bd0be91542
zengchanghuan <zengchanghuan@gmail.com>
2016-07-06 17:52:24 +0800

    轮播图

--- SportClub/SportClub/Classes/Home/View/XZHomeHeaderView.m
+++ SportClub/SportClub/Classes/Home/View/XZHomeHeaderView.m
@@ -21,11 +21,21 @@
     self = [super initWithFrame:frame];
     if (self) {
         
-        NSArray *imagesURLStrings = @[
-                                      @"https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a4b3d7085dee3d6d2293d48b252b5910/0e2442a7d933c89524cd5cd4d51373f0830200ea.jpg",
-                                      @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
-                                      @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
-                                      ];
+        NSArray *imageNames = @[@"h1.jpg",
+                                @"h2.jpg",
+                                @"h3.jpg",
+                                @"h4.jpg",
+                                @"h7" // 本地图片请填写全名
+                                ];
+        SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, XZScreenWidth, 180) shouldInfiniteLoop:YES imageNamesGroup:imageNames];
+        cycleScrollView.delegate = self;
+        cycleScrollView.pageControlStyle = SDCycleScrollViewPageContolStyleClassic;
+        [self addSubview:cycleScrollView];
+        cycleScrollView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        //         --- 轮播时间间隔，默认1.0秒，可自定义
+        //cycleScrollView.autoScrollTimeInterval = 4.0;
+
+        
         
         
         self.backgroundColor = [UIColor grayColor];
